d903f99874d53ae6b2ab35a7620544ce11462e2b
Max Allee <maxwell.w.allee@gmail.com>
2020-11-18 10:19:32 -0500

    cors

--- server.js
+++ server.js
@@ -46,6 +46,15 @@ const getRequest = async (bearer, proxyURL) => {
 // ================================================================================
 app.get('*', async (req, res) => {
   const proxyURL = req.originalUrl;
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
   console.log(proxyURL, '<=========');
   if (proxyURL.includes('favicon.ico') || proxyURL === '/') {
     res.status(200).end();
